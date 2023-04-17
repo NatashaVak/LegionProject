43034f958f0001b786d1219368d3c2f973c4757c
Natalya Vakiryak <vakiryak11@gmail.com>
2023-04-17 19:19:24 -0400

    Changes to be committed: 	modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    'baseURL':'http://167.99.178.249:3000/',
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
